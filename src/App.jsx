0e6e124f712f7397e049a592925ccd6e71467bcb
mary-xn <sembrero.maryjoybscs2024@gmail.com>
2025-04-18 16:01:03 +0800

    For white spaces on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
   return (
       <div className="font-poppins">
       
-      <div className="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)]  bg-[size:20px_20px]">
+      <div className="bg-scroll fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)]  bg-[size:20px_20px]">
 
 
         
