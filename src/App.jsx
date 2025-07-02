5e5eed8e5616115f4534339dd9ea3828e61b6cd5
mary-xn <sembrero.maryjoybscs2024@gmail.com>
2025-07-02 14:07:24 +0800

    Changed bg.

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,8 @@ import Footer from './components/routes/Footer'
 export default function App() {
   return (
     <div className="font-poppins relative min-h-screen">
-      
-      
       <div className="fixed inset-0 -z-10 h-full w-full bg-slate-950">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
-          bg-[size:14px_24px] sm:bg-[size:20px_30px] lg:bg-[size:24px_36px] 
-          transition-all duration-500 ease-in-out">
-        </div>
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       </div>
 
    
